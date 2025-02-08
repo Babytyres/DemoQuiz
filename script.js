ed5ae9768cb1f8a950e77d97ddfa0f6cbb1e6108
Babytyres <117985824+Babytyres@users.noreply.github.com>
2025-02-08 14:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+  "Are you sure about it Maria?😀",
+  "Really sure ah??🤨",
+  "Nalla yoschu sollu...🥰",
+  "Pookie please...😋",
+  "Just think about Maria😉",
+  "If you say no, I will be really sad...😭",
+  "I will be very sad... 🥲",
+  "I will be very very very sad...😔",
+  "Ok fine, I will stop asking...🥹",
+  "Just kidding, say yes please! ❤️",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
